2d3d94edb4d305b899d593eaba8fbc85af7e7fc1
Pragatheeswaran S <pragatheeswaransornarajan@gmail.com>
2020-04-27 09:02:13 +0530

    Delete gulpfile.js

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-
-sass.compiler = require('node-sass');
-gulp.task('sass', function() {
-    return gulp.src('assets/css/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('assets/css/'));
-});
